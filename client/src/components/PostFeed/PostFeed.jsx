348c51aa6d7382378a4ab1c7d2a9c1eae2b2f592
rayGreycloud <raygreycloud@gmail.com>
2021-06-28 11:10:02 -0700

    Scaffold post feed screen

---
+++ client/src/components/PostFeed/PostFeed.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PostFeed = () => {
+  return (
+    <div className='container'>
+      <header className='App-header'>
+        <h1>Public Square</h1>
+      </header>
+    </div>
+  );
+};
+
+export default PostFeed;
