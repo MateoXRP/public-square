1f4ecac4c4ac3d147679bba76ea99748547dd017
rayGreycloud <raygreycloud@gmail.com>
2021-06-28 12:40:02 -0700

    add basic data fetch query

--- client/src/components/PostFeed/PostFeed.jsx
+++ client/src/components/PostFeed/PostFeed.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
+import axios from 'axios';
+import { useQuery, useQueryClient } from 'react-query';
 
 const PostFeed = () => {
+  const queryClient = useQueryClient();
+
+  function usePosts() {
+    return useQuery('posts', async () => {
+      const { data } = await axios.get('/api/posts/test');
+      return data;
+    });
+  }
+
+  const { status, data, error, isFetching } = usePosts();
+  console.log('data: ', data);
   return (
     <div className='container'>
       <header className='App-header'>
         <h1>Public Square</h1>
       </header>
+
+      <div>
+        {status === 'loading' ? (
+          'Loading...'
+        ) : status === 'error' ? (
+          <span>Error: {error.message}</span>
+        ) : (
+          <>
+            <div>
+              <pre>{data.data}</pre>
+            </div>
+            <div>{isFetching ? 'Background Updating...' : ''}</div>
+          </>
+        )}
+      </div>
     </div>
   );
 };
