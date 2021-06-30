259665c7f6e1601a5ca1a9b7c0fb3606cc27a773
rayGreycloud <raygreycloud@gmail.com>
2021-06-30 09:27:18 -0700

    add single post client route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import Post from './components/Post';
 import PostFeed from './components/PostFeed';
 import Topnav from './components/Topnav';
 
@@ -10,6 +11,7 @@ function App() {
       <Router>
         <Topnav />
         <Switch>
+          <Route exact path='/:id' component={Post} />
           <Route exact path='/' component={PostFeed} />
         </Switch>
       </Router>
