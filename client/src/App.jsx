4769010a126a137a0d570d0ee548132047cd48b7
rayGreycloud <raygreycloud@gmail.com>
2021-07-02 11:07:33 -0700

    add client route for Rules page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Post from './components/Post';
 import PostFeed from './components/PostFeed';
+import Rules from './components/Rules';
 import Topnav from './components/Topnav';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Switch>
           <Route exact path='/p/:id' component={Post} />
           <Route exact path='/' component={PostFeed} />
+          <Route exact path='/rules' component={Rules} />
         </Switch>
       </Router>
     </div>
