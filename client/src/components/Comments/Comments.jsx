28d351f35241071f91ffeceabecf87fbad055b17
rayGreycloud <raygreycloud@gmail.com>
2021-07-06 09:22:22 -0700

    Add comments section header

--- client/src/components/Comments/Comments.jsx
+++ client/src/components/Comments/Comments.jsx
@@ -5,41 +5,53 @@ import parseISO from 'date-fns/parseISO';
 const Comments = ({ comments }) => {
   console.log('comments: ', comments);
   return (
-    <ul className='list-group list-group-flush'>
-      {comments.map(comment => {
-        const { account, date, gravatarURL, hash, memoData, username } =
-          comment;
-        const parsedDate = parseISO(date);
-        const timeToNow = formatDistanceToNow(parsedDate);
+    <div className='card-footer'>
+      <p className='h5'>
+        <i class='bi bi-chat-dots-fill'></i>
+        <span className='text-muted ps-3'>{comments.length}</span>
+        <span className='text-muted ps-3'>
+          {comments.length === 1 ? 'Comment' : 'Comments'}
+        </span>
+      </p>
+      <ul className='list-group list-group-flush'>
+        {comments.map(comment => {
+          const { account, date, gravatarURL, hash, memoData, username } =
+            comment;
+          const parsedDate = parseISO(date);
+          const timeToNow = formatDistanceToNow(parsedDate);
 
-        return (
-          <li className='list-group-item' key={hash.substring(hash.length - 8)}>
-            <div className='d-flex align-items-center mb-2'>
-              <img src={gravatarURL} className='rounded-circle' alt='' />
-              <div className='ms-3'>
-                {username ? (
-                  <span className=''>
-                    <span className='card-title'>{username}</span>
-                    <span className='ms-3 text-muted'>{account}</span>
-                  </span>
-                ) : (
-                  <span className=''>{account}</span>
-                )}
+          return (
+            <li
+              className='list-group-item'
+              key={hash.substring(hash.length - 8)}
+            >
+              <div className='d-flex align-items-center mb-2'>
+                <img src={gravatarURL} className='rounded-circle' alt='' />
+                <div className='ms-3'>
+                  {username ? (
+                    <span className=''>
+                      <span className='card-title'>{username}</span>
+                      <span className='ms-3 text-muted'>{account}</span>
+                    </span>
+                  ) : (
+                    <span className=''>{account}</span>
+                  )}
 
-                <div
-                  className='text-muted fs-smaller'
-                  title={parsedDate}
-                >{`${timeToNow} ago`}</div>
+                  <div
+                    className='text-muted fs-smaller'
+                    title={parsedDate}
+                  >{`${timeToNow} ago`}</div>
+                </div>
               </div>
-            </div>
-            <div
-              className='my-3'
-              dangerouslySetInnerHTML={{ __html: memoData }}
-            />
-          </li>
-        );
-      })}
-    </ul>
+              <div
+                className='my-3'
+                dangerouslySetInnerHTML={{ __html: memoData }}
+              />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 };
 
