72ecc2c6cf5b9e2f0361da697b96b39e8c7e54c8
rayGreycloud <raygreycloud@gmail.com>
2021-07-10 10:01:38 -0700

    add faq component and data file

---
+++ client/src/components/FAQ/data-faq.js
@@ -0,0 +1,49 @@
+// data for FAQ
+// array of objects
+// { displayOrder, question, answer }
+
+const faqData = [
+  {
+    displayOrder: 1,
+    question: 'How to create a Post',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, id. Quibusdam magnam amet fugiat corrupti.'
+  },
+  {
+    displayOrder: 2,
+    question: 'How to modify a post or comment',
+    answer: (
+      <div>
+        Posts and comments are part of the transaction data that is saved on the
+        XRP Ledger. Once part of the blockchain, posts and comments are PUBLIC
+        and PERMANENT. They cannot modified or deleted.
+      </div>
+    )
+  },
+  {
+    displayOrder: 3,
+    question: 'How to register username',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, id. Quibusdam magnam amet fugiat corrupti.'
+  },
+  {
+    displayOrder: 4,
+    question: 'How to register email',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, id. Quibusdam magnam amet fugiat corrupti.'
+  },
+  {
+    displayOrder: 5,
+    question: 'How to change avatar',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, id. Quibusdam magnam amet fugiat corrupti.'
+  },
+  {
+    displayOrder: 6,
+    question: 'How to contact support',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, id. Quibusdam magnam amet fugiat corrupti.'
+  }
+];
+
+export { faqData };
