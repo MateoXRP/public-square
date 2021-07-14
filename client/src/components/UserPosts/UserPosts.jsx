45f91f6dd900e60b6444631ea79e6c1f71e0b21a
rayGreycloud <raygreycloud@gmail.com>
2021-07-14 13:21:18 -0700

    add user posts page

---
+++ client/src/components/UserPosts/UserPosts.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { useQuery } from 'react-query';
+
+import PostItem from '../PostItem';
+import Spinner from '../Spinner';
+
+const UserPosts = () => {
+  const { address } = useParams();
+
+  function useUserPosts() {
+    return useQuery('userPosts', async () => {
+      const { data } = await axios.get(`/api/posts/address/${address}`);
+      return data;
+    });
+  }
+
+  const { status, data, error, isFetching } = useUserPosts();
+
+  return (
+    <div className='row justify-content-center '>
+      <div className='col-xs-11 col-sm-10 col-md-8'>
+        <header className='App-header'>
+          <h2 className='text-center display-6 text-light'>Posts By</h2>
+          <h4 className='text-center'>{address}</h4>
+        </header>
+
+        <div>
+          {status === 'loading' ? (
+            <Spinner />
+          ) : status === 'error' ? (
+            <span className='text-danger'>Error: {error.message}</span>
+          ) : (
+            <>
+              <div className=''>
+                {data.posts?.map(post => {
+                  return (
+                    <PostItem
+                      key={post.hash.substring(post.hash.length - 8)}
+                      data={post}
+                    />
+                  );
+                })}
+              </div>
+
+              <div>{isFetching ? <Spinner /> : ''}</div>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserPosts;
