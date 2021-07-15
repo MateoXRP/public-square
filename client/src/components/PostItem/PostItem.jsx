2cc21875baa94d172474c6029aead2f6e766cc8a
rayGreycloud <raygreycloud@gmail.com>
2021-07-15 11:22:54 -0700

    update username display and link to user pages

--- client/src/components/PostItem/PostItem.jsx
+++ client/src/components/PostItem/PostItem.jsx
@@ -1,34 +1,55 @@
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 import parseISO from 'date-fns/parseISO';
-import { Link } from 'react-router-dom';
 
 const PostItem = ({ data }) => {
+  let location = useLocation();
+
   const { account, amount, date, gravatarURL, hash, memoData, username } = data;
   // console.log('account: ', data.post.account);
   const parsedDate = parseISO(date);
   const timeToNow = formatDistanceToNow(parsedDate);
 
+  const Username = username ? (
+    <>
+      <span className='card-title'>{username}</span>
+      <span className='ms-3'>{account}</span>
+    </>
+  ) : (
+    <span>{account}</span>
+  );
+
   return (
     <div className='card my-3'>
       <div className='card-body'>
         <div className='d-flex align-items-center mb-2'>
-          <img src={gravatarURL} className='rounded-circle' alt='' />
+          <img
+            src={gravatarURL}
+            className='rounded-circle img-thumbnail'
+            alt=''
+          />
           <div className='ms-3'>
-            <Link
-              to={`/u/${account}`}
-              className='text-decoration-none'
-              title='See all posts'
-            >
-              {username ? (
-                <>
-                  <span className='card-title'>{username}</span>
-                  <span className='ms-3'>{account}</span>
-                </>
-              ) : (
-                <span>{account}</span>
-              )}
-            </Link>
+            {location.pathname === '/' ? (
+              <Link
+                to={{
+                  pathname: `/u/${account}`,
+                  state: {
+                    user: {
+                      account,
+                      gravatarURL,
+                      username
+                    }
+                  }
+                }}
+                className='text-decoration-none'
+                title='See all posts'
+              >
+                {Username}
+              </Link>
+            ) : (
+              <div>{Username}</div>
+            )}
             <div
               className='text-muted fs-smaller'
               title={parsedDate}
