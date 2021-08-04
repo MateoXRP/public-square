94ec0ec441319d777f042dd4c46a632bf70e2480
rayGreycloud <raygreycloud@gmail.com>
2021-08-04 12:17:23 -0700

    Add config for tip amount settings

---
+++ client/src/config/currency-tip-settings.js
@@ -0,0 +1,15 @@
+const currencyTipSettings = {
+  // currency
+  XRP: {
+    min: 0.1, // minimum tip amount
+    max: 10, // maximum tip amount
+    step: 0.1 // tip input increment
+  },
+  MGS: {
+    min: 1,
+    max: 100,
+    step: 1
+  }
+};
+
+export default currencyTipSettings;
