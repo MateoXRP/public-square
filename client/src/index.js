b23ed5564356d236b1e9ed3c4f7b7164c458c275
rayGreycloud <raygreycloud@gmail.com>
2021-08-05 13:46:38 -0700

    import bootstrap js and scss

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 
-import './index.css';
 import App from './App';
+
+// Bootstap js
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+// styles incl Bootstrap
+import './scss/custom.scss';
 
 const queryClient = new QueryClient();
 
