13c9374d825e80c25313dcfae3c5c084eb6fcebc
rayGreycloud <raygreycloud@gmail.com>
2021-08-10 08:29:32 -0700

    server config cleanup/comments

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,12 @@
+// App env var/API keys
+
 // check environment, if not production
 // load keys from root-level .env file (DO NOT COMMIT FILE)
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
+// export env vars and other non-secret keys
 module.exports = {
   bithompApiKey: process.env.BITHOMP_API_KEY,
   xummApiKey: process.env.XUMM_API_KEY,
