321dd5138a996f27c66b9d4197ae5a2584b7cb81
rayGreycloud <raygreycloud@gmail.com>
2021-10-30 08:52:23 -0700

    update gravatar fn

--- util/user.js
+++ util/user.js
@@ -6,19 +6,23 @@ const getXRPEmailHash = require('../services/xrpscan').getXRPEmailHash;
  * @param {string} account
  * @return {string} gravatarURL
  */
-async function getUserGravatar(account) {
-  try {
-    // generate Gravatar URL
-    const xrpEmailHash = await getXRPEmailHash(account);
+const getUserGravatar = async account =>
+  new Promise(async function (resolve, reject) {
+    try {
+      // generate Gravatar URL
+      const xrpEmailHash = await getXRPEmailHash(account);
 
-    const emailHash = xrpEmailHash ? xrpEmailHash.toLowerCase() : md5(account);
+      const emailHash = xrpEmailHash
+        ? xrpEmailHash.toLowerCase()
+        : md5(account);
 
-    const gravatarURL = `https://www.gravatar.com/avatar/${emailHash}?s=24&d=retro`;
+      const gravatarURL = `https://www.gravatar.com/avatar/${emailHash}?s=24&d=retro`;
 
-    return gravatarURL;
-  } catch (error) {
-    console.log('error: ', error);
-  }
-}
+      resolve(gravatarURL);
+    } catch (error) {
+      console.log('error: ', error);
+      reject(error);
+    }
+  });
 
 module.exports = { getUserGravatar };
